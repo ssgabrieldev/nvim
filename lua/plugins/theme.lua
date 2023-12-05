5759853ecf2ea5a91fc5f405506d8347c2f70985
ssgabrieldev <ssgabrieldev@gmail.com>
2023-12-05 14:51:12 -0300

    nvim config

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,9 +6,10 @@ local M = {
     require("onedark").setup({
       style             = "darker",
       toggle_style_list = { 'darker', 'deep', 'cool', 'warmer', 'dark', 'warm' },
-      toggle_style_key = '<leader>ts',
+      toggle_style_key  = '<leader>ts',
       term_colors       = true,
       ending_tildes     = true,
+      transparent       = true,
       diagnostics       = {
         darker = true,
         undercurl = true,
