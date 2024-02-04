2d21ecbe7842f826c9fbbd9e57a7b7e8544369a8
ssgabrieldev <ssgabrieldev@gmail.com>
2024-02-04 10:21:42 -0300

    nvim: remove java configuration

--- lua/plugins/mason-config.lua
+++ lua/plugins/mason-config.lua
@@ -1,16 +1,14 @@
 local M = {
   "williamboman/mason-lspconfig.nvim",
+  lazy = false,
   dependencies = {
     "neovim/nvim-lspconfig"
   },
-  lazy = false
 }
 
 function M.config()
   require("mason-lspconfig").setup({})
   require("mason-lspconfig").setup_handlers({
-    ["jdtls"] = function()
-    end,
     function(server_name)
       local utils_lsp = require('plugins.utils.lsp')
 
