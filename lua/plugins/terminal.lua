c5b547237a68c26f1add198f0c7ffcefd42ed249
Paulo Gabriel <ssgabrieldev@gmail.com>
2024-07-13 07:28:09 -0300

    ui: open term in float window

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,7 +1,9 @@
 local M = {
   'akinsho/toggleterm.nvim',
   version = "*",
-  opts = {},
+  opts = {
+      direction = "float"
+  },
   keys = {
     { '<c-j>', ':ToggleTerm<cr>', { desc = 'Toggle terminal' } },
     { '<c-j>', '<cmd>ToggleTerm<cr>', mode = 't', { desc = 'Toggle terminal' } },
