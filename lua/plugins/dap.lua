95d891e16d7bc5af3ee188c36a1440b6f00786f7
Paulo Gabriel <ssgabrieldev@gmail.com>
2024-07-13 07:24:10 -0300

    core: add dap-ui dependencie

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -2,6 +2,7 @@ local M = {
   'rcarriga/nvim-dap-ui',
   dependencies = {
     "mfussenegger/nvim-dap",
+    "nvim-neotest/nvim-nio"
   },
   keys = {
     { '<leader>db', ':DapToggleBreakpoint<cr>', { desc = 'Toggle breakpoint' } },
