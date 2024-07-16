a9f82cc68d1657d40c8232c3c1210ecb12a50f97
Paulo Gabriel <ssgabrieldev@gmail.com>
2024-07-16 16:55:54 -0300

    ui: cursorline

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ vim.wo.wrap = false
 vim.wo.number = true
 
 vim.opt.mouse = ""
-vim.opt.cursorline = false
+vim.opt.cursorline = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
