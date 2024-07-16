82ddb7661460994b0463a484b4e54599aecd455b
Paulo Gabriel <ssgabrieldev@gmail.com>
2024-07-16 08:56:30 -0300

    new: :q and :w keybinds

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ vim.wo.wrap = false
 vim.wo.number = true
 
 vim.opt.mouse = ""
-vim.opt.cursorline = true
+vim.opt.cursorline = false
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
@@ -31,8 +31,12 @@ vim.opt.splitright = true
 vim.opt.smartindent = true
 vim.opt.scrolloff = 5
 
-vim.keymap.set('n', '<A-h>', ':vertical resize -2<cr>', { silent = true })
-vim.keymap.set('n', '<A-l>', ':vertical resize +2<cr>', { silent = true })
-vim.keymap.set('n', '<A-j>', ':resize -2<cr>', { silent = true })
-vim.keymap.set('n', '<A-k>', ':resize +2<cr>', { silent = true })
+vim.keymap.set('n', '<A-h>', ':vertical resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<A-l>', ':vertical resize +2<CR>', { silent = true })
+vim.keymap.set('n', '<A-j>', ':resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<A-k>', ':resize +2<CR>', { silent = true })
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>', { silent = true })
+vim.keymap.set('n', '<leader>qq', ':q<CR>', { silent = true })
+vim.keymap.set('n', '<leader>qa', ':qa<CR>', { silent = true })
+vim.keymap.set('n', '<leader>ww', ':w<CR>', { silent = true })
+vim.keymap.set('n', '<leader>wa', ':wa<CR>', { silent = true })
